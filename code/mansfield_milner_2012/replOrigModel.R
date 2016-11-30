0e801ce95ac52887c429753f5a2b6faa48cf8c11
hermes829 <hermes829@gmail.com>
2016-11-29 22:18:41 -0800

    built amen data for mansfield milner

--- code/mansfield_milner_2012/replOrigModel.R
+++ code/mansfield_milner_2012/replOrigModel.R
@@ -17,17 +17,19 @@ baseData <- na.omit(data[, baseVars])
 baseModel <- glm(
 	nohs_rat2_onset2 ~ polity2_04 + polconiiiA + ptadum_new + llntradenew + 
 	llnGDP_gled08 + ldGDP_gled08V2 + larmconflict + latopally + fmrcol_new + 
-	lcontig_new + lndistance + lheg_new + pcw89 + llnGDPratio_g + onsetperc2 + 
-	lnewgatt + nohs2_ptaonspl_r2 + nohs2_ptaonsp1_r2 + nohs2_ptaonsp2_r2 + 
-	nohs2_ptaonsp3_r2 + I(region), 
+	lcontig_new + lndistance + lheg_new + pcw89 + llnGDPratio_g + onsetperc2 + lnewgatt + 
+	nohs2_ptaonspl_r2 + nohs2_ptaonsp1_r2 + nohs2_ptaonsp2_r2 + nohs2_ptaonsp3_r2 + 
+	I(region), 
 	data = baseData, 
 	family = binomial(link = 'logit'))
 
 # need to cluster standard errors by dyads
 loadPkg(c('lmtest','multiwayvcov'))
-baseModelVcov = cluster.vcov(model=baseModel, cluster=baseData$dyadid, 
-	df_correction = FALSE, leverage = 3)
-baseModelSumm = coeftest(baseModel, baseModelVcov)
+baseModelVcov = multiwayvcov::cluster.vcov(
+	model=baseModel, cluster=baseData$dyadid, 
+	df_correction = FALSE, leverage = 3
+	)
+baseModelSumm = lmtest::coeftest(baseModel, baseModelVcov)
 
 # save
 save(baseModelSumm, file=paste0(resultsPath, 'mm_baseModel.rda'))
