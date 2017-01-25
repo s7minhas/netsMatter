b17001f096d94db216932e3e74ed214a1f8d0730
haoliuhoward <haoliuhoward@gmail.com>
2017-01-25 17:31:45 -0500

    howard_rose

---
+++ code/rose2004/runAME_rose.R
@@ -0,0 +1,33 @@
+## run ame model
+#library(devtools)
+#devtools::install_github('s7minhas/amen')
+library(amen)
+
+# load data
+dataPath = "/Users/howardliu/Dropbox/netsMatter/replications/rose2004/"
+load( paste0(dataPath, 'amenData_rose.rda') )
+
+resultsPath = "/Users/howardliu/desktop/"
+
+str(yList)
+yList[1]
+# params
+
+#imps = 50000
+#brn = 100000
+
+imps = 3000
+brn = 1000
+ods = 25
+latDims = 0
+seed=6886
+
+# Run amen in parallel
+
+ameFit = ame_repL(
+	Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
+	model="bin",symmetric=TRUE,intercept=FALSE,R=latDims,
+	nscan=imps, seed=seed, burn=brn, odens=ods,
+	plot=FALSE, print=FALSE, gof=TRUE,
+	periodicSave=TRUE, outFile=paste0(resultsPath,'ameFit_k0.rda')
+	)
