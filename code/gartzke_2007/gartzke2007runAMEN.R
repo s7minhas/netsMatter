55cd648ad7c458bc751ae64a9b98ed80561f2167
Jordan Roberts <jordan.roberts@duke.edu>
2017-04-28 12:36:27 -0400

    New Gartzke

--- code/gartzke_2007/gartzke2007runAMEN.R
+++ code/gartzke_2007/gartzke2007runAMEN.R
@@ -1,6 +1,11 @@
 
 
+<<<<<<< Updated upstream
 if(Sys.info()['user']=='jordan' | Sys.info()['user']=='jordanroberts'){
+=======
+
+if(Sys.info()['user']=='jordan'){
+>>>>>>> Stashed changes
   source('~/netsMatter/code/mansfield_milner_2012/setup.R') }
 
 # load amen data
@@ -114,17 +119,151 @@ save(ameFit,
 
 
 
+<<<<<<< Updated upstream
 # running in parallel varying k
 
+=======
+
+
+
+
+# running just k=0
+
+# params
+imps = 100
+brn = 200
+ods = 10
+latDims = 0
+seed=1014
+
+# Run amen in parallel
+ameFit = ame_repL(
+  Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
+  model="bin",symmetric=TRUE,intercept=FALSE,R=latDims,
+  nscan=imps, seed=seed, burn=brn, odens=ods,
+  plot=FALSE, print=FALSE, gof=TRUE,
+  periodicSave=TRUE,
+  outFile='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k0.rda'
+)
+save(ameFit,
+     file='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k0.rda')
+
+
+# running just k=0
+
+# params
+imps = 100
+brn = 200
+ods = 10
+latDims = 0
+seed=1014
+
+# Run amen in parallel
+ameFit = ame_repL(
+  Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
+  model="bin",symmetric=TRUE,intercept=FALSE,R=latDims,
+  nscan=imps, seed=seed, burn=brn, odens=ods,
+  plot=FALSE, print=FALSE, gof=TRUE,
+  periodicSave=TRUE,
+  outFile='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k0.rda'
+)
+save(ameFit,
+     file='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k0.rda')
+
+
+
+# running just k=1
+
+# params
+imps = 100
+brn = 200
+ods = 10
+latDims = 1
+seed=1014
+
+# Run amen in parallel
+ameFit = ame_repL(
+  Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
+  model="bin",symmetric=TRUE,intercept=FALSE,R=latDims,
+  nscan=imps, seed=seed, burn=brn, odens=ods,
+  plot=FALSE, print=FALSE, gof=TRUE,
+  periodicSave=TRUE,
+  outFile='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k1.rda'
+)
+save(ameFit,
+     file='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k1.rda')
+
+
+# running just k=2
+
+# params
+imps = 100
+brn = 200
+ods = 10
+latDims = 2
+seed=1014
+
+# Run amen in parallel
+ameFit = ame_repL(
+  Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
+  model="bin",symmetric=TRUE,intercept=FALSE,R=latDims,
+  nscan=imps, seed=seed, burn=brn, odens=ods,
+  plot=FALSE, print=FALSE, gof=TRUE,
+  periodicSave=TRUE,
+  outFile='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k2.rda'
+)
+save(ameFit,
+     file='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k2.rda')
+
+
+# running just k=3
+
+# params
+imps = 100
+brn = 200
+ods = 10
+latDims = 3
+seed=1014
+
+# Run amen in parallel
+ameFit = ame_repL(
+  Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
+  model="bin",symmetric=TRUE,intercept=FALSE,R=latDims,
+  nscan=imps, seed=seed, burn=brn, odens=ods,
+  plot=FALSE, print=FALSE, gof=TRUE,
+  periodicSave=TRUE,
+  outFile='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k3.rda'
+)
+save(ameFit,
+     file='/Users/jordan/Dropbox/netsMatter/replications/gartzke2007/Replication\ specific\ data/ameFit_k3.rda')
+
+
+
+
+
+
+
+
+
+
+
+##### running in parallel varying k #####
+>>>>>>> Stashed changes
 # imps = 1000000
 # brn = 500000
 # ods = 25
 # latDims = 0:3
 # seed=6886
 
+<<<<<<< Updated upstream
 imps = 5000
 brn = 10000
 ods = 25
+=======
+imps = 100
+brn = 200
+ods = 10
+>>>>>>> Stashed changes
 latDims = 0:3
 seed=1014
 
