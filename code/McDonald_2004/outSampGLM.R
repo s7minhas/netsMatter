8e525a5104e713806437972dfd17ded660dd485e
hermes829 <hermes829@gmail.com>
2017-07-19 13:57:35 -0400

    weeks cleanup

--- code/McDonald_2004/outSampGLM.R
+++ code/McDonald_2004/outSampGLM.R
@@ -12,8 +12,11 @@ rm(list=ls())
 
 ## load script to load package
 scriptPath <- "../weeks_2012/"
+scriptPath <- '~/Research/netsMatter/code/weeks_2012/'
 
 source(paste0(scriptPath, "LoadPackage.R"))
+char = function(x){ as.character(x) }
+num = function(x){ as.numeric(char(x)) }
 
 library(dplyr)
 library(reshape2)
@@ -21,6 +24,7 @@ library(tidyr)
 ################
 
 dataPath = '~/Dropbox/netsMatter/replications/McDonald_2004/data/'
+dataPath = '~/Dropbox/Research/netsMatter/replications/McDonald_2004/data/'
 ################
 # load data
 
@@ -48,166 +52,99 @@ folds=30
 base_vars<- c('cw2mid', 'cw2midspl','cw2midsp1','cw2midsp2', 'cw2midsp3',
          'ally','cont1', 'lncaprat', 'ldep2l',
          'grow61l', 'lpolity42l','s_wt_glo', 'lrgdpch61h', 'lndistan',
-              'majpow', 'limpduty0200h') %>%  strsplit(x = ., split =
-                                                       " ") %>%  unlist()
-
+              'majpow', 'limpduty0200h') %>%
+    strsplit(x = ., split =" ") %>%  unlist()
 ids = c('ccode1', 'ccode2', 'year', 'dyadid')
 
-form_mod = formula(paste0('value ~ ',
-    paste(base_vars[-1], collapse = '+')))
-
-form_mod
-
-
+form_mod = formula(paste0('value ~ ', paste(base_vars[-1], collapse = '+')))
+glmForm = form_mod
 ################
 
-#glmOutSamp = function(glmForm){
-    
-set.seed(seed)
-    ## Helper function #1:
-    ## Divide dataset into folds
-    
-yListFolds = lapply(yList, function(y){
-    yFold=matrix(sample(1:folds, length(y), replace=TRUE),
-        nrow=nrow(y),ncol=ncol(y), dimnames=dimnames(y))
-    diag(yFold) = NA
-    return(yFold) })
-
-## helper function #2:
-#### run models by fold
-
 ##glmOutSamp = function(glmForm){
-	################
-## divide dataset into folds
 set.seed(seed)
+    ################
+    # divide dataset into folds
+    set.seed(seed)
+    yListFolds = lapply(yList, function(y){
+        yFold=matrix(sample(1:folds, length(y), replace=TRUE),
+            nrow=nrow(y),ncol=ncol(y), dimnames=dimnames(y))
+        diag(yFold) = NA
+        return(yFold) })
+    ################
 
-yListFolds = lapply(yList, function(y){
-    yFold=matrix(sample(1:folds, length(y), replace=TRUE),
-        nrow=nrow(y),ncol=ncol(y), dimnames=dimnames(y))
-    diag(yFold) = NA
-    return(yFold) })
-	################
+    ################
+    # run models by fold
+    yCrossValTrain = lapply(1:folds, function(f){
+        yListMiss = lapply(1:length(yList), function(t){
+            foldID = yListFolds[[t]] ; y = yList[[t]]
+            foldID[foldID==f]=NA ; y=y*foldID 
+            return(y) })
+        names(yListMiss) = names(yList)
+        return(yListMiss) }) ; names(yCrossValTrain) = char(1:folds)
 
-	################
-	# run models by fold
-	yCrossValTrain = lapply(1:folds, function(f){
-            yListMiss = lapply(1:length(yList), function(t){
-                foldID = yListFolds[[t]]
-                y = yList[[t]]
-                foldID[foldID==f]=NA
-                y=y*foldID
-                return(y) })
+    # melt into glm format
+    yCrossValTrain = lapply(yCrossValTrain, function(y){
+        y = melt(y)
+        xd = dcast(melt(xDyadList), Var1 + Var2 + L1 ~ Var3)
+        xd = xd[order(xd$L1, xd$Var2, xd$Var1),]
 
-            names(yListMiss) = names(yList)
-            return(yListMiss) }) ; names(yCrossValTrain) = as.character(1:folds)
+        glmData = cbind(y,xd)
+        glmData = glmData[which(glmData$Var1!=glmData$Var2),]
+        return(glmData) 
+    })
 
-class(yCrossValTrain)
-## step through the melt into glm format
-## fold by fold
-
-y=yCrossValTrain[[1]]
-
-class(y) ## list, entry is for years
-attributes(y)
-
-	## melt into glm format
-	###yCrossValTrain = lapply(yCrossValTrain, function(y){
-          ##  print("**")
-            y = melt(y) ## turns list into a df with vars of dyad ids, value, and L1= year
-xd = melt(xDyadList)## makes a year list into a dyad data-frame
-xd = tidyr::spread(xd, key = Var3, value = value) ## independent variables by dyad, 18k x 18
-##print(head(xd))
-glmData = cbind(y,xd[,base_vars[-1]]) 
-glmData = glmData[which(glmData$Var1!=glmData$Var2),]
-## return(glmData)
-##	})
-
-head(glmData)
-
-names(yCrossValTrain) = as.character(1:folds)
-print(names(yCrossValTrain))
-
-
-        ## helper function #3:
-    ## run glm
-    
+    # run glm
     fitCrossVal = lapply(yCrossValTrain, function(glmData){
-        print("*")
         glmData$value[glmData$value>1] = 1
-        fit = glm(form_mod, data=glmData, family='binomial')	
+        fit = glm(glmForm, data=glmData, family='binomial') 
         return(fit)
     })
 
-attributes(fitCrossVal)
-class(fitCrossVal)
+    # get preds
+    outPerf = do.call('rbind', lapply(1:folds, function(f){
+        # get probs
+        testData = cbind(int=1,yCrossValTrain[[f]][is.na(yCrossValTrain[[f]]$value),names(coef(fitCrossVal[[f]]))[-1]])
+        testData = na.omit(testData)
+        prob = 1/(1+exp(-as.matrix(testData) %*% coef(fitCrossVal[[f]])))
 
-    ## get predictions
-    
-    #outPerf = do.call('rbind', lapply(1:folds, function(f){
-        ## print counter
-     #   print("#")
-## get probs
-f=1 #for the debugging
-
-testData =# leading 1s, then 
-    cbind(int=1,yCrossValTrain[[f]][is.na(yCrossValTrain[[f]]$value),
-              names(coef(fitCrossVal[[f]]))[-1]]) 
-prob = 1/(1+exp(-as.matrix(testData) %*% coef(fitCrossVal[[f]])))
-                                        #Logit link
-
-class(testData)
-        ## print(head(prob))
-        
-## get actual
-## MJF: parse through "actual" to debug it:
-## first line: for each entry in yListFolds:
-
-actual=unlist(lapply(1:length(yListFolds), function(t){
-    
-    foldID = yListFolds[[t]] ; y = yList[[t]]
-    foldID[foldID!=f]=NA ; foldID[!is.na(foldID)] = 1
-    y=c(y*foldID) ; return(y[!is.na(y)])
-}))
-
-
+        # get actual
+        actual=unlist(lapply(1:length(yListFolds), function(t){
+                foldID = yListFolds[[t]] ; y = yList[[t]]
+                foldID[foldID!=f]=NA ; foldID[!is.na(foldID)] = 1
+                y=c(y*foldID) ; return(y[!is.na(y)])
+            }))
         if(length(actual)!=length(prob)){stop('shit went wrong.')}
-        res = data.frame(actual=actual, pred=prob, fold=f,
-            stringsAsFactors = FALSE)
-        
-        ##if(any(grepl('lagDV',glmOutSamp))){res=na.omit(res)}
+        res = data.frame(actual=actual, pred=prob, fold=f, stringsAsFactors = FALSE)
         return(res)
     }))
-    
-    ## Helper function #4
-    ## get binperfhelpers
+        
+    # get binperfhelpers
     loadPkg(c('ROCR', 'RColorBrewer', 'caTools'))
-    source('binPerf.R')
-        ## helper function #5:
-    ## get performance stats    
+    source(paste0(scriptPath, 'binPerf.R'))
+
+    # get perf stats
     aucByFold=do.call('rbind', lapply(1:folds, function(f){
         slice = outPerf[outPerf$fold==f,]
+        slice = na.omit(slice)
         if(length(unique(slice$actual))==1){ return(NULL) }
         perf=cbind(fold=f,
             aucROC=getAUC(slice$pred, slice$actual),
             aucPR=auc_pr(slice$actual, slice$pred)
             )
         return(perf) } ))
-
     aucROC=getAUC(outPerf$pred, outPerf$actual)
     aucPR=auc_pr(outPerf$actual, outPerf$pred)
-    
-    ##Format to Return
+    ################
+
+    ################
     out = list( yCrossValTrain=yCrossValTrain,
         fitCrossVal=fitCrossVal,
         outPerf=outPerf, aucByFold=aucByFold,
         aucROC=aucROC, aucPR=aucPR )
-    
     return(out)
+    ################    
 }
 ###############
-
-form_mod
-ls()
 
 ## think that this is what I want to run, when I package
 ## the above back into a function
