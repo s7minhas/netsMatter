1c38348fec796003e1ffd2d5d9dbee3f4bd1a9e0
Howard Liu <haoliuhoward@gmail.com>
2017-07-27 13:53:59 -0400

    howard updates

--- code/rose2004/2a_ouSam_amen.R
+++ code/rose2004/2a_ouSam_amen.R
@@ -1,112 +1,140 @@
 ################
+rm(list=ls())
+
 # workspace
-if(Sys.info()['user']=='janus829' | Sys.info()['user']=='s7m'){ source('~/Research/conflictEvolution/R/setup.R')  }
-if(Sys.info()['user']=='cassydorff' | Sys.info()['user']=='cassydorff'){ source('~/ProjectsGit/conflictEvolution/R/setup.R')  }
-if(Sys.info()['user']=='maxgallop'){ source('~/Documents/conflictEvolution/R/setup.R')  }
-loadPkg('devtools') ; devtools::install_github('s7minhas/amen') ; library(amen)
+# if(Sys.info()['user']=='janus829' | Sys.info()['user']=='s7m'){ source('~/Research/conflictEvolution/R/setup.R')  }
+# if(Sys.info()['user']=='cassydorff' | Sys.info()['user']=='cassydorff'){ source('~/ProjectsGit/conflictEvolution/R/setup.R')  }
+# if(Sys.info()['user']=='maxgallop'){ source('~/Documents/conflictEvolution/R/setup.R')  }
+# loadPkg('devtools') ; devtools::install_github('s7minhas/amen') ; library(amen)
 ################
+
+# load pkg
+source("clusteredSE.R")
+source("LoadPkg.R")
+packs = c('foreign','dplyr', 'ggplot2', 'readr', 'lmtest','multiwayvcov', 'amen')
+loadPkg(packs)
+
+# some short-handed functions
+char = function(x){ as.character(x) }
+num = function(x){ as.numeric(char(x)) }
+trim = function (x) { gsub("^\\s+|\\s+$", "", x) }
 
 ################
 # load data
-load(paste0(pathResults, 'ameResults.rda'))
+pathResults = "/Users/howardliu/Dropbox/netsMatter/replications/rose2004/"
+load(paste0(pathResults, 'amenData_rose.rda'))
+
+resultsPath = "/Users/howardliu/Dropbox/netsMatter/replications/rose2004/outputData/"
+load(paste0(resultsPath, 'ameFit_k3_v1.rda')); ameFit_k3 = ameFit
+
+load(paste0(resultsPath, 'ameFit_k2_v1.rda')); ameFit_k2 = ameFit
+
+load(paste0(resultsPath, 'ameFit_k1_v1.rda')); ameFit_k1 = ameFit ##??
+
+load(paste0(resultsPath, 'ameFit_k0_v1.rda')); ameFit_k0 = ameFit
+
 ################
 
 ################
 # function to run k-fold cross validation analysis using ame
 ameOutSamp = function(
-	yList, xDyadL=NULL, xRowL=NULL, xColL=NULL, startVals, ## startVal (to pick one portion)
-	seed=6886, folds=30,
-	R=2, model='bin', burn=10000, nscan=2000, odens=25, # burn=1000, nscan= 2000, odens=10, just save time starting from low
-	intercept=TRUE, rvar=TRUE, cvar=TRUE, symmetric=FALSE
-	){
+  yList, xDyadL=NULL, xRowL=NULL, xColL=NULL, startVals, ## startVal (to pick one portion)
+  seed=6886, folds=30,
+  R=2, model='nrm', burn=10000, nscan=2000, odens=25, # burn=1000, nscan= 2000, odens=10, just save time starting from low
+  intercept=TRUE, rvar=TRUE, cvar=TRUE, symmetric=FALSE
+){
 
-	################
-	# divide dataset into folds
-	set.seed(seed)
-	yListFolds = lapply(yList, function(y){
-		yFold=matrix(sample(1:folds, length(y), replace=TRUE),
-			nrow=nrow(y),ncol=ncol(y), dimnames=dimnames(y)) # Na --> affects folds, so only 4-5 folds
-		diag(yFold) = NA
-		return(yFold) }) # num of folds
-	################
+  ################
+  # divide dataset into folds
+  set.seed(seed)
+  yListFolds = lapply(yList, function(y){
+    yFold=matrix(sample(1:folds, length(y), replace=TRUE),
+                 nrow=nrow(y),ncol=ncol(y), dimnames=dimnames(y)) # Na --> affects folds, so only 4-5 folds
+    diag(yFold) = NA
+    return(yFold) }) # num of folds
+  ################
 
-	################
-	# run models by fold
-	yCrossValTrain = lapply(1:folds, function(f){ # setup list of training dataset
-		yListMiss = lapply(1:length(yList), function(t){ # going to YList
-			foldID = yListFolds[[t]] ; y = yList[[t]]
-			foldID[foldID==f]=NA ; y=y*foldID # foldID[foldID==f]=NA ; foldID[!is.na(foldID)]=1 ; y=y*foldID line 38 (normal model)
-			return(y) })
-		names(yListMiss) = names(yList)
-		return(yListMiss) }) ; names(yCrossValTrain) = char(1:folds)
+  ################
+  # run models by fold
+  yCrossValTrain = lapply(1:folds, function(f){ # setup list of training dataset
+    yListMiss = lapply(1:length(yList), function(t){ # going to YList
+      foldID = yListFolds[[t]] ; y = yList[[t]]
+      foldID[foldID==f]=NA ;foldID[!is.na(foldID)]=1; y=y*foldID # foldID[foldID==f]=NA ; foldID[!is.na(foldID)]=1 ; y=y*foldID line 38 (normal model)
+      return(y) })
+    names(yListMiss) = names(yList)
+    return(yListMiss) }) ; names(yCrossValTrain) = char(1:folds)
 
-	# run ame by fold
-	fitCrossVal = lapply(yCrossValTrain, function(yCV){
-		fit=ame_repL(
-			Y=yCV, Xdyad=xDyadL, Xrow=xRowL, Xcol=xColL, ## make sure start value low (seting huge Net model to NA)
-			symmetric=symmetric, rvar=rvar, cvar=cvar, R=R,
-			model=model, intercept=intercept, seed=seed,
-			burn=burn, nscan=nscan, odens=odens,
-			plot=FALSE, gof=TRUE, periodicSave=FALSE,
-			startVals=startVals
-			)
-		return(fit) })
+  # run ame by fold
+  fitCrossVal = lapply(yCrossValTrain, function(yCV){
+    fit=ame_repL(
+      Y=yCV, Xdyad=xDyadL, Xrow=xRowL, Xcol=xColL, ## make sure start value low (seting huge Net model to NA)
+      symmetric=symmetric, rvar=rvar, cvar=cvar, R=R,
+      model=model, intercept=intercept, seed=seed,
+      burn=burn, nscan=nscan, odens=odens,
+      plot=FALSE, gof=TRUE, periodicSave=FALSE,
+      startVals=startVals
+    )
+    return(fit) })
 
-	# get preds (predictions)
-	outPerf = do.call('rbind', lapply(1:folds, function(f){
-		fitFoldPred = fitCrossVal[[f]]$'EZ'
-		do.call('rbind', lapply(1:length(fitFoldPred), function(t){
-			predT = fitFoldPred[[t]]
-			foldID = yListFolds[[t]] ; y = yList[[t]]
-			foldID[foldID!=f]=NA ; foldID[!is.na(foldID)] = 1
-			y=y*foldID ; predT=predT*foldID ## change here for normal!!!
-			res=na.omit(data.frame(actual=c(y), pred=c(predT), fold=f, stringsAsFactors=FALSE))
-			#res$pred = 1/(1+exp(-res$pred)) --> normal model
-			return(res) }) ) }) )
+  # get preds (predictions)
+  outPerf = do.call('rbind', lapply(1:folds, function(f){
+    fitFoldPred = fitCrossVal[[f]]$'EZ'
+    do.call('rbind', lapply(1:length(fitFoldPred), function(t){
+      predT = fitFoldPred[[t]]
+      foldID = yListFolds[[t]] ; y = yList[[t]]
+      foldID[foldID!=f]=NA ; foldID[!is.na(foldID)] = 1
+      y=y*foldID ; predT=predT*foldID ## change here for normal!!!
+      res=na.omit(data.frame(actual=c(y), pred=c(predT), fold=f, stringsAsFactors=FALSE))
+      #res$pred = 1/(1+exp(-res$pred)) --> normal model
+      return(res) }) ) }) )
 
-	# get binperfhelpers
-	loadPkg(c('ROCR', 'RColorBrewer', 'caTools'))
-	source(paste0(fPth, 'binPerfHelpers.R'))
+  # get binperfhelpers
+  rmse = sqrt(mean((outPerf$actual - outPerf$pred)^2, na.rm = T))
+  rmdse = sqrt(median((outPerf$actual - outPerf$pred)^2, na.rm = T))
+  ssTOT = sum((outPerf$actual - mean(outPerf$actual, na.rm = T))^2, na.rm = T)
+  ssRES = sum((outPerf$pred - outPerf$actual)^2, na.rm = T)
+  rmseByFold=do.call('rbind', lapply(1:folds, function(f){
+    slice = outPerf[outPerf$fold==f,]
+    if(length(unique(slice$actual))==1){ return(NULL) }
+    perf=cbind(fold=f,
+               rmse=sqrt(mean((slice$actual - slice$pred)^2, na.rm = T)),
+               rmdse =sqrt(median((slice$actual - slice$pred)^2, na.rm = T)),
+               r2 =1 - sum((slice$pred - slice$actual)^2, na.rm = T)/sum((slice$actual - mean(slice$actual, na.rm = T))^2, na.rm = T))
+    return(perf) } ))
 
-	# get perf stats  (just do RMSE)
-	aucByFold=do.call('rbind', lapply(1:folds, function(f){
-		slice = outPerf[outPerf$fold==f,]
-		if(length(unique(slice$actual))==1){ return(NULL) }
-		perf=cbind(fold=f,
-			aucROC=getAUC(slice$pred, slice$actual),
-			aucPR=auc_pr(slice$actual, slice$pred)
-			)
-		return(perf) } ))
-	aucROC=getAUC(outPerf$pred, outPerf$actual)
-	aucPR=auc_pr(outPerf$actual, outPerf$pred)
-	################
-
-	# org output and return
-	out=list(
-		yCrossValTrain=yCrossValTrain,
-		fitCrossVal=fitCrossVal,
-		outPerf=outPerf, aucByFold=aucByFold,
-		aucROC=aucROC, aucPR=aucPR
-	)
-	return(out)
+  r2 = 1 - ssRES/ssTOT
+  out = list( yCrossValTrain=yCrossValTrain,
+              fitCrossVal=fitCrossVal,
+              outPerf=outPerf, rmseByFold=rmseByFold,
+              rmseOUT = rmse, rmdseOUT = rmdse )
+  return(out)
 }
 ################
 
 ################
 # run outsamp models
-ameOutSamp_NULL = ameOutSamp(
-	yList=yList, xDyadL=NULL, xRowL=NULL, xColL=NULL,
-	startVals=fit$startVals
-	)
 
-ameOutSamp_wFullSpec = ameOutSamp(
-	yList=yList, xDyadL=xDyadL, xRowL=xRowL, xColL=xColL,
-	startVals=fitFullSpec$startVals
-	)
-
+# k = 3
+ameOutSamp_k3 = ameOutSamp(
+  yList=yList, xDyadL=xDyadList, xRowL=NULL, xColL=NULL,
+  startVals=ameFit_k3$'startVals', # k = 3
+  folds = 4,
+  burn=2000, nscan=4000, odens=10
+)
 # save
-save(
-	ameOutSamp_NULL, ameOutSamp_wFullSpec,
-	file=paste0(pathResults, 'ameCrossValResults.rda')
-	)
-################
+save(ameOutSamp_k3,
+  file=paste0(pathResults, 'ameCrossValResults_k3.rda')
+)
+
+ameOutSamp_k3$rmseOUT #  1.967908
+
+# k = 2
+ameOutSamp_k2 = ameOutSamp(
+  yList=yList, xDyadL=xDyadList, xRowL=NULL, xColL=NULL,
+  startVals=ameFit_k2$'startVals',
+  folds = 4,
+  burn=2000, nscan=4000, odens=10
+) # take 3 hours
+
+ameOutSamp_k2$rmseOUT # 1.987901 
+
