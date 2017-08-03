94bb0320f02e12c46238c1fbe239b69618f0c2cc
Howard Liu <haoliuhoward@gmail.com>
2017-08-03 14:00:05 -0400

    make sure symmetric

--- code/rose2004/buildAME_rose2004.R
+++ code/rose2004/buildAME_rose2004.R
@@ -1,98 +1,100 @@
 ## build AME input data for rose 2004
+rm(list=ls())
 
-source("/Users/howardliu/Dropbox/netsRep_Howard_Max/rose2004/clusteredSE.R")
-source("/Users/howardliu/Dropbox/netsRep_Howard_Max/rose2004/LoadPkg.R")
 
-packs = c('base','foreign','dplyr', 'ggplot2', 'readr', 'lmtest','multiwayvcov','reshape2')
+# load pkg
+source("clusteredSE.R")
+source("LoadPkg.R")
+packs = c('foreign','dplyr', 'ggplot2', 'readr', 'lmtest','multiwayvcov')
 loadPkg(packs)
 
-# misc
+# some short-handed functions
 char = function(x){ as.character(x) }
 num = function(x){ as.numeric(char(x)) }
 trim = function (x) { gsub("^\\s+|\\s+$", "", x) }
 
+# load replication data
 dataPath = '/Users/howardliu/Dropbox/netsMatter/replications/rose2004/'
 data = read.dta(paste0(dataPath,"data4web.dta"))
 
 # get sampling frame
-baseVars = c('cty1', 'cty2', 'year', 'pairid', 'ltrade', # log real trade
+baseVars = c('cty1', 'cty2', 'year', 'pairid', 'ltrade',
         'bothin', 'onein', 'gsp', 'ldist', 'lrgdp', 'lrgdppc', 'regional',
         'custrict',
         'comlang', 'border', 'landl', 'island', 'lareap', 'comcol', 'curcol', 'colony', 'comctry'
         )
 
-
 dataComp = na.omit(data[, baseVars])
 years = sort(unique(dataComp$year)) %>% as.character()
-cntriesT = lapply(years, function(t){ char( unique( dataComp$cty1[dataComp$year==t] ) ) }) # make sure they are even matrices
+cntriesT = lapply(years, function(t){ char( unique( dataComp$cty1[dataComp$year==t] ) ) }) # to make sure they are even matrices
 
 
-# dv (shah's code)
-data = select(data, cty1, cty2, year, pairid, ltrade, bothin, onein, gsp, ldist, lrgdp, lrgdppc, regional, custrict, comlang, border, landl, island, lareap, comcol, curcol, colony, comctry, year)
+# DV data
+# data = dplyr::select(data, cty1, cty2, year, pairid, ltrade, bothin, onein, gsp, ldist, lrgdp, lrgdppc, regional, custrict, comlang, border, landl, island, lareap, comcol, curcol, colony, comctry, year)
 data$year = data$year %>% as.character()
+
 yVar = 'ltrade'
 
-ii  = 1
+ii = 1
+
 yList = lapply(1:length(years), function(ii){
-    slice = data[ which(
-            data$year==years[ii] &
-            data$cty1 %in% cntriesT[[ii]] &
-            data$cty2 %in% cntriesT[[ii]]
-            ), c('cty1', 'cty2', yVar) ]
-    missa = unique(slice$cty1[!slice$cty1 %in% slice$cty2])
-    missb = unique(slice$cty2[!slice$cty2 %in% slice$cty1])
-    add = cbind(missb, missa, rep(NA, max(length(missa), length(missb)))) # add into slice
-    #add
-    colnames(add) = names(slice)
-    slice = rbind(slice, add) # add in
-    adj = reshape2::acast(slice, cty1 ~ cty2, value.var=yVar)
-    #adj
-    # adj[lower.tri(adj)] <- 0
-    adj[lower.tri(adj)] = adj[upper.tri(adj)]
+    slice = data[ which( data$year==years[ii] &
+        data$cty1 %in% cntriesT[[ii]] &
+        data$cty2 %in% cntriesT[[ii]]
+    ), c('cty1', 'cty2', yVar) ]
+    head(slice)
+    # flip order
+    slice_flip = slice
+    slice_flip$cty1 = slice$cty2
+    slice_flip$cty2 = slice$cty1
+    head(slice_flip)
+    sliceL = rbind(slice, slice_flip)
+    adj = reshape2::acast(sliceL, cty1 ~ cty2, value.var=yVar)
     return( adj[ cntriesT[[ii]], cntriesT[[ii]] ] )
 })
+
+# make sure symmetric
+yList[[1]]
+for(yr in 1:length(yList)){
+  print(isSymmetric(yList[[yr]]))
+}
+
+k = adj %>% as.data.frame()
 
 
 # dyadic vars
 dVars_mod1 = c(
         'bothin', 'onein', 'gsp', 'ldist', 'lrgdp', 'lrgdppc', 'regional',
         'custrict',
-        'comlang', 'border', 'landl', 'island', 'lareap', 'comcol', 'curcol', 'colony', 'comctry'
-        )
+        'comlang', 'border', 'landl', 'island', 'lareap', 'comcol', 'curcol', 'colony', 'comctry')
 
-# var = c('ltrade', # log real trade
-#         'bothin', 'onein', 'gsp', 'ldist', 'lrgdp', 'lrgdppc', 'regional',
-#         'custrict',
-#         'comlang', 'border', 'landl', 'island', 'lareap', 'comcol', 'curcol', 'colony', 'comctry', 'as.factor(year)'
-#         )
 
 xDyadList = lapply(1:length(years), function(ii){
-	slice = data[ which(
+	 slice = data[ which(
 			data$year==years[ii] &
 			data$cty1 %in% cntriesT[[ii]] &
 			data$cty2 %in% cntriesT[[ii]]
 			), c('cty1', 'cty2', dVars_mod1) ]
-	  #head(slice)
-	  missa = unique(slice$cty1[!slice$cty1 %in% slice$cty2])
-    #missa
-	  missb = unique(slice$cty2[!slice$cty2 %in% slice$cty1])
-    #missb
-	  add = cbind(missb, missa)
-    #add
-	  add_cov = matrix(NA, max(length(missa), length(missb)), length(dVars_mod1))
-	  #add_cov
-	  add = cbind(add,add_cov)
-	  #add
-	  colnames(add) = names(slice)
-    slice = rbind(slice, add)
-	sliceL = reshape2::melt(slice, id=c('cty1','cty2'))
-	adj = reshape2::acast(sliceL, cty1 ~ cty2 ~ variable, value.var='value')
-	# adj[lower.tri(adj)] <- 0 # this seems wrong, try the below, sm
-    for(p in 1:dim(adj)[3]){ x=adj[,,p] ; x[lower.tri(x)] = x[upper.tri(x)] ; adj[,,p]=x ; rm(x) }
-	return( adj[ cntriesT[[ii]], cntriesT[[ii]],  ] )
+    # flip order
+    slice_flip = slice
+    slice_flip$cty1 = slice$cty2
+    slice_flip$cty2 = slice$cty1
+    #head(slice_flip)
+    sliceL = rbind(slice, slice_flip)
+	  sliceL = reshape2::melt(sliceL, id=c('cty1','cty2'))
+	  adj = reshape2::acast(sliceL, cty1 ~ cty2 ~ variable, value.var='value')
+
+	  return( adj[ cntriesT[[ii]], cntriesT[[ii]],  ] )
 })
+
+# make sure symmetric
+yr = 50
+for(v in 1:17){
+  print( isSymmetric( xDyadList[[yr]][,,v] ) )
+}
+
+k = adj[,,10] %>% as.data.frame()
 
 
 # save dfs
 save(yList, xDyadList, file=paste0(dataPath, 'amenData_rose.rda'))
-
