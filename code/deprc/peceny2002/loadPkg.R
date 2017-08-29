d1952555bcf13e3f686343cb86759bcd98842462
s7minhas <s7.minhas@gmail.com>
2017-08-29 13:33:29 -0400

    dir cleanup

--- code/deprc/peceny2002/loadPkg.R
+++
@@ -1,7 +0,0 @@
-## Function to install and load packages
-loadPkg=function(toLoad){
-  for(lib in toLoad){
-    if(! lib %in% installed.packages()[,1])
-    { install.packages(lib, repos='http://cran.rstudio.com/') }
-    suppressMessages( library(lib, character.only=TRUE) ) }
-}
