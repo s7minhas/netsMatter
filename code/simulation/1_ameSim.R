72ea9814a6c0d3d7abe16d745c4671b453352580
s7minhas <s7.minhas@gmail.com>
2017-08-29 21:32:21 -0400

    cleanup

--- code/simulation/1_ameSim.R
+++ code/simulation/1_ameSim.R
@@ -1,67 +1,90 @@
+##############################
 rm(list=ls())
 
 if(Sys.info()['user']=='s7m'){
 	fPath = '~/Research/netsMatter/code/helpers/'
 	dPath = '~/Dropbox/Research/netsMatter/'
 	simResPath = paste0(dPath, 'simulation/')
-	source(paste0(fPath, 'functions.R'))
-}
+	source(paste0(fPath, 'functions.R')) }
 
 toLoad = c(
 	'devtools', 
 	'foreach', 'doParallel',
-	'magrittr', 'dplyr', 'ggplot2'
-	)
+	'magrittr', 'dplyr', 'ggplot2' )
 loadPkg(toLoad)
 devtools::install_github('s7minhas/amen', ref='dev')
+##############################
 
+##############################
 ### Homophily
-set.seed(6886)
+simRun = function(seed, n, mu, beta, gamma){
+	set.seed(seed) 
+	xw = matrix(rnorm(n*2),n,2)
+	X = tcrossprod(xw[,1])  ; W = tcrossprod(xw[,2])
+	XW = array(dim=c(n,n,2)) ; XW[,,1] = X ; XW[,,2] = W
 
-simRun = function(NSIM, n, mu, beta, gamma, cores){
+	# create DV
+	Y<- 1*(mu + beta*X + gamma*W + matrix(rnorm(n*n),n,n) >0)
 
-	# prlz
-	cl = makeCluster(cores) ; registerDoParallel(cl)	
-	simResults = foreach(sim = 1:NSIM, .packages=c('amen')) %dopar% {
-		set.seed(sim) 
-		xw = matrix(rnorm(n*2),n,2)
-		X = tcrossprod(xw[,1]) 
-		W = tcrossprod(xw[,2])
-		XW = array(dim=c(n,n,2)) ; XW[,,1] = X ; XW[,,2] = W
+	# run AME models
+	fit0 = ame(Y,X,R=0,rvar=FALSE,cvar=FALSE,dcor=FALSE,model='bin',
+		print=FALSE,plot=FALSE)
+	fit1 = ame(Y,X,R=1,rvar=FALSE,cvar=FALSE,dcor=FALSE,model='bin',
+		print=FALSE,plot=FALSE)
+	fitO = ame(Y,XW,R=0,rvar=FALSE,cvar=FALSE,dcor=FALSE,model='bin',
+		print=FALSE,plot=FALSE)
 
-		# create DV
-		Y<- 1*(mu + beta*X + gamma*W + matrix(rnorm(n*n),n,n) >0)
+	# gather together results
+	beta = list(naive=fit0$BETA, ame=fit1$BETA, oracle=fitO$BETA)
+	uv = list(naive=fit0$UVPM, ame=fit1$UVPM, oracle=fitO$UVPM)
+	u = list(naive=fit0$U, ame=fit1$U, oracle=fitO$U)
+	v = list(naive=fit0$V, ame=fit1$V, oracle=fitO$V) 
+	gof = list(naive=fit0$GOF, ame=fit1$GOF, oracle=fitO$GOF)
+	out = list(beta=beta, uv=uv, u=u, v=v, gof=gof)	
+	return(out) }
+##############################
 
-		# run AME models
-		fit0 = ame(Y,X,R=0,rvar=FALSE,cvar=FALSE,dcor=FALSE,model='bin',
-			print=FALSE,plot=FALSE)
-		fit1 = ame(Y,X,R=1,rvar=FALSE,cvar=FALSE,dcor=FALSE,model='bin',
-			print=FALSE,plot=FALSE)
-		fitO = ame(Y,XW,R=0,rvar=FALSE,cvar=FALSE,dcor=FALSE,model='bin',
-			print=FALSE,plot=FALSE)
-
-		# gather together results
-		beta = list(naive=fit0$BETA, ame=fit1$BETA, oracle=fitO$BETA)
-		uv = list(naive=fit0$UVPM, ame=fit1$UVPM, oracle=fitO$UVPM)
-		u = list(naive=fit0$U, ame=fit1$U, oracle=fitO$U)
-		v = list(naive=fit0$V, ame=fit1$V, oracle=fitO$V) 
-		gof = list(naive=fit0$GOF, ame=fit1$GOF, oracle=fitO$GOF)
-
-		out = list(beta=beta, uv=uv, u=u, v=v, gof=gof)
-		return(out) }
-	return(simResults) }
+##############################
+# params
+imps = 100
+intEff=-2 ; x1Eff=1 ; x2Eff=1
 
 #
-imps = 1000 ; cores = 7
-intEff =  -2 ; x1Eff = 1 ; x2Eff = 1
-ameSim30 = simRun(imps, 30, intEff, x1Eff, x2Eff, cores)
-ameSim50 = simRun(imps, 30, intEff, x1Eff, x2Eff, cores)
-ameSim100 = simRun(imps, 30, intEff, x1Eff, x2Eff, cores)
+writeLines(c(''), paste0(simResPath,'ameSim30Log.txt'))
+cl=makeCluster(7) ; registerDoParallel(cl)
+ameSim30 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
+	out=simRun(seed=imp, n=30, mu=intEff, beta=x1Eff, gamma=x2Eff)
+	writeLines(
+		paste0(imp, ' out of ', imps, ' completed: ', round(imp/imps,2)*100, '%'),
+		paste0(simResPath,'ameSim30Log.txt'), append=TRUE
+		)
+	return(out) }  ; stopCluster(cl)
+save( ameSim30, file=paste0(simResPath, 'ameSim30.rda') )
 
 #
-save(
-	ameSim30, 
-	ameSim50, 
-	ameSim100, 
-	file=paste0(simResPath, 'ameSim.rda')
-	)
+writeLines(c(''), paste0(simResPath,'ameSim50Log.txt'))
+cl=makeCluster(7) ; registerDoParallel(cl)
+ameSim50 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
+	out=simRun(seed=imp, n=50, mu=intEff, beta=x1Eff, gamma=x2Eff)
+	writeLines(
+		paste0(imp, ' out of ', imps, ' completed: ', round(imp/imps,2)*100, '%'),
+		paste0(simResPath,'ameSim50Log.txt'), append=TRUE
+		)	
+	return(out) } ; stopCluster(cl)
+save( ameSim50, file=paste0(simResPath, 'ameSim50.rda') )
+
+#
+writeLines(c(''), paste0(simResPath,'ameSim100Log.txt'))
+cl=makeCluster(7) ; registerDoParallel(cl)
+ameSim50 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
+	out=simRun(seed=imp, n=100, mu=intEff, beta=x1Eff, gamma=x2Eff)
+	writeLines(
+		paste0(imp, ' out of ', imps, ' completed: ', round(imp/imps,2)*100, '%'),
+		paste0(simResPath,'ameSim100Log.txt'), append=TRUE
+		)	
+	return(out) } ; stopCluster(cl)
+save( ameSim100, file=paste0(simResPath, 'ameSim100.rda') )
+
+#
+save(ameSim30, ameSim50, ameSim100, file=paste0(simResPath, 'ameSim.rda'))
+##############################
