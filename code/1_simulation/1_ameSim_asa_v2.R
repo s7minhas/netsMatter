15cab5e765eefdd4b89c8bfa544e4c720fc51582
s7minhas <s7.minhas@gmail.com>
2020-06-04 16:00:11 -0400

    sync up sim code

---
+++ code/1_simulation/1_ameSim_asa_v2.R
@@ -0,0 +1,184 @@
+##############################
+rm(list=ls())
+
+if(Sys.info()['user']=='s7m'){
+	fPath = '~/Research/netsMatter/code/helpers/'
+	dPath = '~/Dropbox/Research/netsMatter/'
+	simResPath = paste0(dPath, 'simulation/')
+	source(paste0(fPath, 'functions.R')) }
+
+if(Sys.info()['user'] %in% c('herme','Owner','S7M')){
+	base=paste0('C:/Users/',Sys.info()['user'],'/')
+	gPath = paste0(base, 'Research/netsMatter/')
+	fPath = paste0(gPath, 'code/helpers/')
+	dPath = paste0(base, 'Dropbox/Research/netsMatter/')
+	simResPath = paste0(dPath, 'simulation/')
+	graphicsPath = paste(gPath, 'paper/')
+	source(paste0(fPath, 'functions.R')) }
+
+toLoad = c(
+	'devtools',
+	'foreach', 'doParallel',
+	'magrittr', 'dplyr', 'ggplot2' )
+loadPkg(toLoad)
+devtools::install_github('s7minhas/amen')
+library(amen)
+##############################
+
+n= 50
+seed=1
+mu=1
+beta=1
+gamma=.25
+
+##############################
+### Homophily
+# simRun = function(seed, n, mu, beta, gamma){
+
+	##############
+	# pull data gen code directly from asa
+	index <- 1:n
+	P <- choose(n,2)
+
+	# Dyads
+	dyads <-  as.matrix(
+		apply(t(combn(index, 2)), 1,
+			function(x)paste(x[1], x[2],sep="-")))
+
+	dyad.mat <- t(apply(dyads, 1, function(x)unlist(strsplit(x,"-"))))
+
+	set.seed(seed)
+	m = function(x){matrix(x, ncol=2, nrow=1)}
+
+	# gen some variables
+	a <- rnorm(n)
+	X <- rnorm(n)
+
+	# Misspec
+	dX <- da <- NA
+	for(i in 1:P){
+		da[i] <- sum(a[as.numeric(dyad.mat[i,])])
+		dX[i] <- diff(X[as.numeric(dyad.mat[i,])])
+	}
+
+	# specify Y
+	dX2 = dX^2
+	dY <- mu*da + beta*dX + gamma*dX2 + rnorm(P)
+
+	# create df
+	dataUp <- data.frame(
+		dyads, dY, da, dX, dX2,
+		dyad1 = as.numeric(dyad.mat[,1]),
+		dyad2 = as.numeric(dyad.mat[,2]),
+		stringsAsFactors=FALSE
+	)
+	##############
+
+	##############
+	### build data for ame
+	## create adj mat representation of y and x
+	actors = unique(c(dataUp$dyad1, dataUp$dyad2))
+	n=length(actors)
+	yMatrix = matrix(NA,
+		nrow=n, ncol=n,
+		dimnames=list(actors, actors) )
+	xMatrix = array(NA,
+		dim=c(n,n,3),
+		dimnames=list(actors, actors, c('da','dX','dX2')) )
+
+	# iterate through data and fill in adjMats
+	for(iii in 1:nrow(dataUp)){
+		# slice up data
+		slice = dataUp[iii,,drop=FALSE]
+		a1 = char(slice$dyad1) ; a2 = char(slice$dyad2)
+
+		# fill in dv
+		yMatrix[a1, a2] = slice$dY
+		yMatrix[a2, a1] = slice$dY
+
+		# fill in dyadic covariate
+		xMatrix[a1,a2,'da'] = slice$da
+		xMatrix[a2,a1,'da'] = slice$da
+		xMatrix[a1,a2,'dX'] = slice$dX
+		xMatrix[a2,a1,'dX'] = slice$dX
+		xMatrix[a1,a2,'dX2'] = slice$dX2
+		xMatrix[a2,a1,'dX2'] = slice$dX2
+	}
+	##############
+
+	##############
+	# run AME models
+	fit0 = ame(
+		yMatrix,xMatrix[,,'dX',drop=FALSE],
+		R=0,rvar=FALSE,cvar=FALSE,symmetric=TRUE,model='nrm',
+		print=FALSE,plot=FALSE)
+	fit1 = ame(
+		yMatrix,xMatrix[,,'dX',drop=FALSE],
+		R=1,rvar=TRUE,cvar=TRUE,symmetric=TRUE,model='nrm',
+		print=FALSE,plot=FALSE)
+	fit2 = ame(
+		yMatrix,xMatrix[,,'dX',drop=FALSE],
+		R=2,rvar=TRUE,cvar=TRUE,symmetric=TRUE,model='nrm',
+		print=FALSE,plot=FALSE)
+	fit4 = ame(
+		yMatrix,xMatrix[,,'dX',drop=FALSE],
+		R=4,rvar=TRUE,cvar=TRUE,symmetric=TRUE,model='nrm',
+		print=FALSE,plot=FALSE)
+	fit8 = ame(
+		yMatrix,xMatrix[,,'dX',drop=FALSE],
+		R=8,rvar=TRUE,cvar=TRUE,symmetric=TRUE,model='nrm',
+		print=FALSE,plot=FALSE)
+	fitO = ame(
+		yMatrix,xMatrix,
+		R=0,rvar=TRUE,cvar=TRUE,symmetric=TRUE,model='nrm',
+		print=FALSE,plot=FALSE)
+	##############
+
+	# gather together results
+	beta = list(naive=fit0$BETA,
+		ame1=fit1$BETA, ame2=fit2$BETA,
+		ame4=fit4$BETA, ame8=fit8$BETA,
+		oracle=fitO$BETA)
+	lapply(beta, function(x){apply(x,2,mean)})
+	uv = list(naive=fit0$UVPM, ame=fit1$UVPM, oracle=fitO$UVPM)
+	u = list(naive=fit0$U, ame=fit1$U, oracle=fitO$U)
+	v = list(naive=fit0$V, ame=fit1$V, oracle=fitO$V)
+	gof = list(naive=fit0$GOF, ame=fit1$GOF, oracle=fitO$GOF)
+	out = list(beta=beta, uv=uv, u=u, v=v, W=W, gof=gof)
+	return(out) }
+##############################
+
+##############################
+# params
+imps = 1000 ; cores = 21
+intEff=-2 ; x1Eff=1 ; x2Eff=1
+n= 50
+seed=1
+mu=1
+beta=1
+gamma=.25
+
+#
+cl=makeCluster(cores) ; registerDoParallel(cl)
+ameSim30 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
+	out=simRun(seed=imp, n=30, mu=intEff, beta=x1Eff, gamma=x2Eff)
+	return(out) }  ; stopCluster(cl)
+save( ameSim30, file=paste0(simResPath, 'ameSim30_asa.rda') )
+
+#
+cl=makeCluster(cores) ; registerDoParallel(cl)
+ameSim50 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
+	out=simRun(seed=imp, n=50, mu=intEff, beta=x1Eff, gamma=x2Eff)
+	return(out) } ; stopCluster(cl)
+save( ameSim50, file=paste0(simResPath, 'ameSim50_asa.rda') )
+
+#
+cl=makeCluster(cores) ; registerDoParallel(cl)
+ameSim100 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
+	out=simRun(seed=imp, n=100, mu=intEff, beta=x1Eff, gamma=x2Eff)
+	return(out) } ; stopCluster(cl)
+save( ameSim100, file=paste0(simResPath, 'ameSim100_asa.rda') )
+
+
+save(ameSim30, ameSim50, ameSim100, file=paste0(simResPath, 'ameSim_asa.rda'))
+##############################
