4a6a8b736eda4fe14f7945780480cedfed93a0ad
s7minhas <s7.minhas@gmail.com>
2020-07-18 08:06:53 -0400

    sync up

--- code/1_simulation/1_ameSim.R
+++ code/1_simulation/1_ameSim.R
@@ -19,9 +19,9 @@ if(Sys.info()['user'] %in% c('herme','Owner','S7M')){
 toLoad = c(
 	'devtools',
 	'foreach', 'doParallel',
-	'magrittr', 'dplyr', 'ggplot2' )
+	'magrittr', 'dplyr' )
 loadPkg(toLoad)
-devtools::install_github('s7minhas/amen', ref='dev')
+library(amen)
 ##############################
 
 ##############################
@@ -33,7 +33,7 @@ simRun = function(seed, n, mu, beta, gamma){
 	XW = array(dim=c(n,n,2)) ; XW[,,1] = X ; XW[,,2] = W
 
 	# create DV
-	Y<- 1*(mu + beta*X + gamma*W + matrix(rnorm(n*n),n,n) >0)
+	Y = 1*(mu + beta*X + gamma*W + matrix(rnorm(n*n),n,n) >0)
 
 	# run AME models
 	fit0 = ame(Y,X,R=0,rvar=FALSE,cvar=FALSE,dcor=FALSE,model='bin',
@@ -55,40 +55,20 @@ simRun = function(seed, n, mu, beta, gamma){
 
 ##############################
 # params
-imps = 1000
+imps = 100
 intEff=-2 ; x1Eff=1 ; x2Eff=1
 
 #
-getTime = function(){format(Sys.time(), '%X')}
-writeLines(c('n=30 using 8 cores\n'), paste0(simResPath,'ameSim30Log.txt'))
-cl=makeCluster(8) ; registerDoParallel(cl)
-ameSim30 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
-	out=simRun(seed=imp, n=30, mu=intEff, beta=x1Eff, gamma=x2Eff)
-	sink(paste0(simResPath,'ameSim30Log.txt'), append=TRUE)
-	cat(paste0(imp, ' out of ', imps, ' completed: ', getTime(),'\n'))
-	return(out) }  ; stopCluster(cl)
-save( ameSim30, file=paste0(simResPath, 'ameSim30.rda') )
-
-#
-writeLines(c('n=50 using 8 cores\n'), paste0(simResPath,'ameSim50Log.txt'))
-cl=makeCluster(8) ; registerDoParallel(cl)
+cl=makeCluster(20) ; registerDoParallel(cl)
 ameSim50 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
 	out=simRun(seed=imp, n=50, mu=intEff, beta=x1Eff, gamma=x2Eff)
-	sink(paste0(simResPath,'ameSim50Log.txt'), append=TRUE)
-	cat(paste0(imp, ' out of ', imps, ' completed: ', getTime(),'\n'))
 	return(out) } ; stopCluster(cl)
 save( ameSim50, file=paste0(simResPath, 'ameSim50.rda') )
 
 #
-writeLines(c('n=100 using 8 cores\n'), paste0(simResPath,'ameSim100Log.txt'))
-cl=makeCluster(8) ; registerDoParallel(cl)
+cl=makeCluster(20) ; registerDoParallel(cl)
 ameSim100 = foreach(imp = 1:imps, .packages=c('amen')) %dopar% {
 	out=simRun(seed=imp, n=100, mu=intEff, beta=x1Eff, gamma=x2Eff)
-	sink(paste0(simResPath,'ameSim100Log.txt'), append=TRUE)
-	cat(paste0(imp, ' out of ', imps, ' completed: ', getTime(),'\n'))
 	return(out) } ; stopCluster(cl)
 save( ameSim100, file=paste0(simResPath, 'ameSim100.rda') )
-
-#
-# save(ameSim30, ameSim50, ameSim100, file=paste0(simResPath, 'ameSim.rda'))
 ##############################
