6bd80105b645fda9b6e9f3879cbb18c60d1dc2b6
s7m <s7.minhas@gmail.com>
2021-03-14 20:47:36 -0400

    cleaned crossval code

--- replArchive/2_applications/reiter_stam_crossval.R
+++ replArchive/2_applications/reiter_stam_crossval.R
@@ -8,9 +8,30 @@ rsPth = paste0(pth, '2_applications/application_data/reiter_stam/')
 # to get same results
 # devtools::install_github('s7minhas/amen', ref='pa2018_version')
 library(amen)
+
+# load in helper functions for ameOutSamp
+source(paste0(pth, 'helpers/functions.R'))
+loadPkg(c('ROCR', 'RColorBrewer', 'caTools'))
+source(paste0(pth, 'helpers/binPerfHelpers.r'))
+source(paste0(pth, 'helpers/ameOutSamp.R'))
 ##############################
 
 ##############################
 # load data
 load(paste0(rsPth, 'reiterStamData.rda'))
+load(paste0(rsPth, 'ameFitReiterStam.rda'))
+startVals0 = ameFit$startVals ; rm(ameFit)
+##############################
+
+##############################
+# run out samp code and save
+startTime = Sys.time()
+ameOutSamp_k2 = ameOutSamp(
+  yList=yList, xDyadL=xDyadList, xRowL=NULL, xColL=NULL,
+  R=2, model='bin',
+  startVals=startVals0,
+  folds = 30, cores=10 )
+endTime = Sys.time()
+save(ameOutSamp_k2, startTime, endTime,
+  file=paste0(rsPth, 'ameOutSampReiterStam.rda'))
 ##############################
