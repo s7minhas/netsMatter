ead74fa43394ae46eeca96c1628f0fb084d2cce3
s7m <s7.minhas@gmail.com>
2021-03-16 21:35:35 -0400

    stdzing glm crossval code across repls

--- replArchive/2_applications/reiter_stam_ameCrossval.R
+++ replArchive/2_applications/reiter_stam_ameCrossval.R
@@ -2,15 +2,12 @@
 # set a path
 require(here)
 pth = paste0(here::here(), '/replArchive/')
-fPth = paste0(pth, '/helpers/')
-wPth = paste0(pth, '2_applications/application_data/weeks/')
+rsPth = paste0(pth, '2_applications/application_data/reiter_stam/')
 
 # install github from specific repo
 # to get same results
 # devtools::install_github('s7minhas/amen', ref='pa2018_version')
 library(amen)
-rZ_bin_fc2 <- dget(paste0(fPth, "rZBinfc.R"))
-assignInNamespace("rZ_bin_fc", rZ_bin_fc2, pos="package:amen")
 
 # load in helper functions for ameOutSamp
 source(paste0(pth, 'helpers/functions.R'))
@@ -21,22 +18,20 @@ source(paste0(pth, 'helpers/ameOutSamp.R'))
 
 ##############################
 # load data
-load(paste0(wPth, 'weeksData.rda'))
-load(paste0(wPth, 'ameFitWeeks.rda'))
+load(paste0(rsPth, 'reiterStamData.rda'))
+load(paste0(rsPth, 'ameFitReiterStam.rda'))
 startVals0 = ameFit$startVals ; rm(ameFit)
 ##############################
 
 ##############################
-# run out samp code and save
+# run out samp code and save ~14 hours
 startTime = Sys.time()
 ameOutSamp_k2 = ameOutSamp(
-  yList=yList, xDyadL=xDyadList,
-  xRowL=xNodeList.s,
-  xColL=xNodeList.r,
+  yList=yList, xDyadL=xDyadList, xRowL=NULL, xColL=NULL,
   R=2, model='bin',
   startVals=startVals0,
-  folds = 30, cores=10, rzBinFix=TRUE, fPth=fPth )
+  folds = 30, cores=10 )
 endTime = Sys.time()
 save(ameOutSamp_k2, startTime, endTime,
-  file=paste0(wPth, 'ameOutSampWeeks.rda'))
+  file=paste0(rsPth, 'ameOutSampReiterStam.rda'))
 ##############################
---
+++ replArchive/2_applications/reiter_stam_glmCrossval.R
@@ -0,0 +1,149 @@
+#############################
+# set a path
+require(here)
+pth = paste0(here::here(), '/replArchive/')
+fPth = paste0(pth, 'helpers/')
+rsPth = paste0(pth, '2_applications/application_data/reiter_stam/')
+
+#
+source(paste0(fPth, 'loadPkg.R'))
+packs = c('reshape2', 'tidyr', 'ROCR', 'RColorBrewer', 'caTools')
+loadPkg(packs)
+
+# misc
+char = function(x){ as.character(x) }
+num = function(x){ as.numeric(char(x)) }
+trim = function (x) { gsub("^\\s+|\\s+$", "", x) }
+
+#
+source(paste0(fPth, 'binPerfHelpers.R'))
+##############################
+
+##############################
+# load data
+load(paste0(rsPth, 'reiterStamData.rda'))
+##############################
+
+# params
+seed = 6886
+folds = 30
+cores=10
+# yList
+# xDyadList
+# xNodal.S
+# xnodal.R
+
+##############################
+# create object with similar length
+# and dimensions as yList but the values
+# within correspond to the fold that obs
+# are sorted into for the out of samp analysis
+set.seed(seed)
+yListFolds = lapply(yList, function(y){ # assign each y-matrix to a fold
+  yFold=matrix(sample(1:folds, length(y), replace=TRUE),
+               nrow=nrow(y),ncol=ncol(y), dimnames=dimnames(y))
+  diag(yFold) = NA
+  return(yFold) })
+##############################
+
+##############################
+# divide dataset up into folds where each fold
+# contains observations from each fold except
+# a particular fold which will serve as the
+# holdout sample for perf testing
+yCrossValTrain = lapply(1:folds, function(f){
+  yListMiss = lapply(1:length(yList), function(t){
+    foldID = yListFolds[[t]] ; y = yList[[t]]
+    foldID[foldID==f]=NA ; y=y*foldID
+    return(y) })
+  names(yListMiss) = names(yList)
+  return(yListMiss) }) ; names(yCrossValTrain) = as.character(1:folds)
+##############################
+
+##############################
+# add nodal covariates to dyadic covariates
+
+
+# melt list of covariate arrays
+# into a data.frame
+xd = reshape2::melt(xDyadList)
+xd = tidyr::spread(xd, key = Var3, value = value)
+xd$id = paste(xd$Var1, xd$Var2, xd$L1, sep='_')
+xd =  xd[which(xd$Var1 != xd$Var2),]
+
+# get vector of ivs
+ivs = dimnames(xDyadList[[1]])[[3]]
+##############################
+
+##############################
+# melt y down into a list of dyadic dfs and merge in covariates
+yCrossValTrain = lapply(yCrossValTrain, function(y){
+  y = reshape2::melt(y) ; y$id = paste(y$Var1, y$Var2, y$L1, sep='_')
+  y = y[which(y$Var1!=y$Var2),]
+  matchedIDs = match(y$id, xd$id)
+  y = cbind(y, xd[matchedIDs,ivs])
+  return(y) })
+##############################
+
+##############################
+# generate model formula and run glm
+modForm = formula(
+  paste0("value ~ ", paste(ivs, collapse='+')))
+fitCrossVal = lapply(yCrossValTrain, function(glmData){
+  glmData$value[glmData$value>1] = 1
+  fit = glm(modForm, data=glmData, family='binomial')
+  return(fit) })
+##############################
+
+##############################
+# get preds
+f = 1
+t = 1
+
+
+outPerf = do.call('rbind', lapply(1:folds, function(f){
+  # get probs
+  testData = cbind(
+    int=1,
+    yCrossValTrain[[f]][
+        is.na(yCrossValTrain[[f]]$value),
+        names(coef(fitCrossVal[[f]]))[-1]
+      ]
+    )
+  prob = 1/(1+exp(-as.matrix(testData) %*% coef(fitCrossVal[[f]])))
+
+  # get actual
+  actual=unlist(lapply(1:length(yListFolds), function(t){
+    foldID = yListFolds[[t]] ; y = yList[[t]]
+    foldID[foldID!=f]=NA ; foldID[!is.na(foldID)] = 1
+    # y=c(y*foldID) ; return(y[!is.na(y)])
+    return( y[!is.na(foldID)] )
+  }))
+  if(length(actual)!=length(prob)){stop('shit went wrong.')}
+  res = data.frame(actual=actual, pred=prob, fold=f, stringsAsFactors = FALSE)
+  if(any(grepl('lagDV',modForm))){res=na.omit(res)}
+  return(res)
+}))
+##############################
+
+##############################
+# get perf stats
+aucByFold=do.call('rbind', lapply(1:folds, function(f){
+  slice = na.omit(outPerf[outPerf$fold==f,])
+  if(length(unique(slice$actual))==1){ return(NULL) }
+  perf=cbind(fold=f,
+             aucROC=getAUC(slice$pred, slice$actual),
+             aucPR=auc_pr(slice$actual, slice$pred)
+  )
+  return(perf) } ))
+outPerf = na.omit(outPerf)
+aucROC=getAUC(outPerf$pred, outPerf$actual)
+aucPR=auc_pr(outPerf$actual, outPerf$pred)
+##############################
+
+##############################
+# organize output
+glmOutSamp_wFullSpec = list(
+  outPerf=outPerf, aucByFold=aucByFold,
+  aucROC=aucROC, aucPR=aucPR )
+##############################
