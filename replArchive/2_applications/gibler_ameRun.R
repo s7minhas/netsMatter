1e7107d78868d1903216ec00c3a37ed9cb84b307
s7m <s7.minhas@gmail.com>
2021-03-19 00:02:00 -0400

    fixing up crossval code

--- replArchive/2_applications/gibler_ameRun.R
+++ replArchive/2_applications/gibler_ameRun.R
@@ -12,20 +12,24 @@ library(amen)
 
 ##############################
 # load ame version of data
-load(paste0(gPth, 'giblerData.rda'))
+load(paste0(gPth, 'giblerData_v2.rda'))
+# load(paste0(gPth, 'giblerData.rda'))
+# load(paste0(gPth, 'ameFitGibler_v2.rda'))
+# startVals0 = ameFit$startVals
+# save(yList, xDyadList, startVals0, file=paste0('giblerData_v2.rda'))
 ##############################
 
 ##############################
-# run ame and save ~2 days
+# run ame and save ~4 days
 startTime = Sys.time()
 ameFit = ame_repL(
 	Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
 	model="bin",symmetric=TRUE, # MID DV: undirected data
 	R=2,
 	nscan=100000, seed=6886, burn=25000, odens=25,
-	plot=FALSE, print=FALSE,
+	plot=FALSE, print=FALSE, periodicSave=TRUE,
 	startVals = startVals0 )
 endTime = Sys.time()
 print( endTime-startTime )
-save(ameFit, startTime, endTime, file=paste0(gPth, 'ameFitGibler_v2.rda'))
+save(ameFit, startTime, endTime, file=paste0(gPth, 'ameFitGibler_v3.rda'))
 ##############################
