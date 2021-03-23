5d9b51fabca3b5143923c48f2aa2c914390bbd73
s7m <s7.minhas@gmail.com>
2021-03-23 13:31:38 -0400

    crossval analysis

--- replArchive/2_applications/gibler_ameCrossval.R
+++ replArchive/2_applications/gibler_ameCrossval.R
@@ -19,7 +19,7 @@ source(paste0(pth, 'helpers/ameOutSamp.R'))
 ##############################
 # load ame version of data
 load(paste0(gPth, 'giblerData.rda'))
-load(paste0(gPth, 'ameFitGibler_v2.rda'))
+load(paste0(gPth, 'ameFitGibler.rda'))
 startVals0 = ameFit$startVals ; rm(ameFit)
 ##############################
 
@@ -30,7 +30,7 @@ ameOutSamp_k2 = ameOutSamp(
   yList=yList, xDyadL=xDyadList, xRowL=NULL, xColL=NULL,
   R=2, model='bin',
   startVals=startVals0,
-  folds = 30, cores=10 )
+  folds = 30, cores=20 )
 endTime = Sys.time()
 save(ameOutSamp_k2, startTime, endTime,
   file=paste0(rsPth, 'ameOutSampGibler.rda'))
