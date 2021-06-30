7478aeae629af3f9c8577648d5e9548cc0507e00
s7m <s7.minhas@gmail.com>
2021-06-30 15:50:59 -0400

    edits to readme for psrm replicators

--- replArchive/helpers/binPerfHelpers.R
+++ replArchive/helpers/binPerfHelpers.R
@@ -11,25 +11,25 @@ roc = function(prediction, actual){
 
 # Auc, depends ROCR
 getAUC = function(prediction, actual){
-	pred = prediction(prediction, actual)	
+	pred = prediction(prediction, actual)
 	attributes(performance(pred,"auc"))$y.values[[1]]
 }
 
 # Plot roc curves, depends RColorBrewer
 # plot_type is "roc" or "pr"
 rocPlot = function(
-  rocData, type='roc', legPos=c(.56,.25), colorPal = 'Set1', 
+  rocData, type='roc', legPos=c(.56,.25), colorPal = 'Set1',
   colorManual=NULL, linetypes, legText=6, legSpace=3){
 
-	if(type=='roc'){ 
-    tmp=ggplot(rocData, aes(x=FPR, y=TPR, color=model, linetype=model)) + 
-      geom_abline(intercept=0, slope=1, color='darkgrey') + 
+	if(type=='roc'){
+    tmp=ggplot(rocData, aes(x=FPR, y=TPR, color=model, linetype=model)) +
+      geom_abline(intercept=0, slope=1, color='darkgrey') +
       ylab('True Positive Rate (Sensitivity)') + xlab('False Positive Rate (1-Specificity)')
 
   }
 
-  if(type=='pr'){ 
-    tmp=ggplot(rocData, aes(x=rec, y=prec, color=model, linetype=model)) + 
+  if(type=='pr'){
+    tmp=ggplot(rocData, aes(x=rec, y=prec, color=model, linetype=model)) +
       ylab('Precision') + xlab('Recall (True Positive Rate)')
   }
 
@@ -39,19 +39,19 @@ rocPlot = function(
     tmp = tmp + scale_color_manual(values=colorManual)
   }
 
-	tmp=tmp + 
+	tmp=tmp +
     geom_line(lwd=1) +
-    ylim(0,1) + 
-    scale_linetype_manual(values=linetypes) + 
-    # theme_light(base_family="Source Sans Pro") + 
+    ylim(0,1) +
+    scale_linetype_manual(values=linetypes) +
+    # theme_light(base_family="Source Sans Pro") +
     theme(
   		legend.position=legPos, legend.title=element_blank(),
-      legend.background=element_blank(), 
+      legend.background=element_blank(),
       legend.text.align = 0, legend.text=element_text(size=legText),
       legend.key=element_rect(colour = NA, fill = NA), legend.key.size=unit(legSpace,'lines'),
       # axis.text.x=element_text(family="Source Sans Pro Light"),
-      # axis.text.y=element_text(family="Source Sans Pro Light"),    
-      axis.ticks=element_blank(),    
+      # axis.text.y=element_text(family="Source Sans Pro Light"),
+      axis.ticks=element_blank(),
   		panel.border=element_blank()
 		)
 	return(tmp)
@@ -63,19 +63,19 @@ ggSep = function(actual, proba, color, lty, fPath, save=TRUE){
   color = c('white',color)
   sepData = data.frame(actual, proba)
   sepData = sepData[order(sepData$proba),]
-  tmp=ggplot(sepData) + 
+  tmp=ggplot(sepData) +
     geom_rect(
-      aes(xmin = 0, xmax = seq(length.out = length(actual)), 
+      aes(xmin = 0, xmax = seq(length.out = length(actual)),
         ymin = 0, ymax = 1), fill = "transparent") +
     geom_linerange(
-      aes(color = factor(actual), 
+      aes(color = factor(actual),
         ymin = 0, ymax = 1, x = seq(length.out = length(actual))), alpha = 0.5) +
-    geom_line(aes(y = proba, x = seq(length.out = length(actual)), linetype=lty), lwd = 4) + 
+    geom_line(aes(y = proba, x = seq(length.out = length(actual)), linetype=lty), lwd = 4) +
     scale_linetype_manual(values=lty) +
-    scale_color_manual(values=color) + 
-    scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0), breaks=seq(0,1,.25)) + 
+    scale_color_manual(values=color) +
+    scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0), breaks=seq(0,1,.25)) +
     theme(
-      legend.position='none', 
+      legend.position='none',
       panel.grid=element_blank(), panel.border=element_rect(colour = "grey13"),
       axis.ticks=element_blank(),
       axis.text=element_blank(),
@@ -92,7 +92,7 @@ ggSepBig = function(actual, proba, color, lty, fPath, cutBy=0.001, save=TRUE){
   sepData$cutVar = cut(sepData$proba, breaks=quantile(sepData$proba, seq(0, 1, cutBy)) )
   sepDataSumm = sepData %>% group_by(cutVar) %>%
     dplyr::summarize(
-      actual=sum(actual), 
+      actual=sum(actual),
       proba=mean(proba) )
 
   #
@@ -100,21 +100,21 @@ ggSepBig = function(actual, proba, color, lty, fPath, cutBy=0.001, save=TRUE){
   color = c('white', rep(color, cats))
   sepDataSumm = sepDataSumm[order(sepDataSumm$proba),]
 
-  tmp=ggplot(sepDataSumm) + 
+  tmp=ggplot(sepDataSumm) +
     geom_rect(
-      aes(xmin = 0, xmax = seq(length.out = length(actual)), 
+      aes(xmin = 0, xmax = seq(length.out = length(actual)),
         ymin = 0, ymax = 1), fill = "transparent") +
     geom_linerange(
       aes(
         size=factor(actual), color=factor(actual), ymin=0, ymax=1, x=seq(length.out=length(actual))
         ), alpha = 0.5) +
-    geom_line(aes(y = proba, x = seq(length.out = length(actual)), linetype=lty), lwd = 4) + 
+    geom_line(aes(y = proba, x = seq(length.out = length(actual)), linetype=lty), lwd = 4) +
     scale_linetype_manual(values=lty) +
-    scale_color_manual(values=color) + 
+    scale_color_manual(values=color) +
     scale_size_manual( values=c(.5,rep(2, cats)) ) +
-    scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0), breaks=seq(0,1,.25)) + 
+    scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0), breaks=seq(0,1,.25)) +
     theme(
-      legend.position='none', 
+      legend.position='none',
       panel.grid=element_blank(), panel.border=element_rect(colour = "grey13"),
       axis.ticks=element_blank(),
       axis.text=element_blank(),
@@ -149,7 +149,7 @@ rocdf <- function(pred, obs, data=NULL, type=NULL) {
   if (!is.null(data)) {
     pred <- eval(substitute(pred), envir=data)
     obs  <- eval(substitute(obs), envir=data) }
-  
+
   rocr_xy <- switch(type, roc=c("tpr", "fpr"), pr=c("prec", "rec"))
   rocr_df <- prediction(pred, obs)
   rocr_pr <- performance(rocr_df, rocr_xy[1], rocr_xy[2])
@@ -160,7 +160,8 @@ rocdf <- function(pred, obs, data=NULL, type=NULL) {
 
 ####################################################################
 #
-ggPerfCurves = function(predDfs, suffix, cutBy=0.001){
+ggPerfCurves = function(predDfs, suffix, plotPath, cutBy=0.001){
+
   # tabular data
   loadPkg('PRROC')
   aucSumm=do.call('rbind', lapply(predDfs,function(x){
@@ -171,14 +172,14 @@ ggPerfCurves = function(predDfs, suffix, cutBy=0.001){
   aucSumm = trim(format(round(aucSumm, 2), nsmall=2))
 
   # roc data
-  rocData=do.call('rbind', 
+  rocData=do.call('rbind',
     lapply(predDfs, function(x){
       y = roc.curve(x$pred[x$actual==1], x$pred[x$actual==0], curve=T)$curve[,1:2]
       y = data.frame(y, stringsAsFactors = FALSE) ; names(y) = c('FPR','TPR')
       y$model=unique(x$model);return(y) }))
 
   # precision recall curve data
-  prData=do.call('rbind', 
+  prData=do.call('rbind',
     lapply(predDfs, function(x){
       y = pr.curve(x$pred[x$actual==1], x$pred[x$actual==0], curve=T)$curve[,1:2]
       y = data.frame(y, stringsAsFactors = FALSE) ; names(y) = c('rec','prec')
@@ -201,10 +202,11 @@ ggPerfCurves = function(predDfs, suffix, cutBy=0.001){
   })
 
   # get rid of null model
-  rocData$model = factor(rocData$model, levels=levels(rocData$model))
-  prData$model = factor(prData$model, levels=levels(prData$model))
+  modLabs = unlist(lapply(predDfs, function(x){unique(x$model)}))
+  rocData$model = factor(rocData$model, levels=modLabs)
+  prData$model = factor(prData$model, levels=modLabs)
 
-  tmp = rocPlot(rocData, linetypes=ggLty, colorManual=ggCols) + 
+  tmp = rocPlot(rocData, linetypes=ggLty, colorManual=ggCols) +
     guides(linetype = FALSE, color = FALSE)
   yLo = -.04 ; yHi = .14
   for(ii in 1:length(sepPngList)){
@@ -216,13 +218,13 @@ ggPerfCurves = function(predDfs, suffix, cutBy=0.001){
   tmp=rocPlot(
     prData, type='pr', legText=12, legPos=c(.25,.35),
     legSpace=2, linetypes=ggLty, colorManual=ggCols) +
-    guides(linetype=FALSE, color=FALSE) + 
-    annotate('text', hjust=0, x=c(.6, .77, .91), y=.85, 
-      label=c('  ', ' AUC\n(ROC)', 'AUC\n(PR)'), size=4) + 
-    annotate('text', hjust=0, x=.6, y=c(0.65,.75), 
-      label=rev(rownames(aucSumm))) + 
-    annotate('text', hjust=0, x=.78, y=c(0.65,.75), 
+    guides(linetype=FALSE, color=FALSE) +
+    annotate('text', hjust=0, x=c(.6, .77, .91), y=.85,
+      label=c('  ', ' AUC\n(ROC)', 'AUC\n(PR)'), size=4) +
+    annotate('text', hjust=0, x=.6, y=c(0.65,.75),
+      label=rev(rownames(aucSumm))) +
+    annotate('text', hjust=0, x=.78, y=c(0.65,.75),
       label=rev(apply(aucSumm, 1, function(x){paste(x, collapse='    ')})), size=4 )
-  ggsave(tmp, file=paste0(plotPath, suffix, '_pr_outSample.pdf'), width=5, height=5)  
-}  
+  ggsave(tmp, file=paste0(plotPath, suffix, '_pr_outSample.pdf'), width=5, height=5)
+}
 ####################################################################
