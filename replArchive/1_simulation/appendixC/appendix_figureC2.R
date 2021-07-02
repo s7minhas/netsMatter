e3aa8751943756fae74592edf64b2a4b126619ee
s7m <s7.minhas@gmail.com>
2021-07-02 15:54:04 -0400

    cleanup

--- replArchive/1_simulation/appendixC/appendix_figureC2.R
+++ replArchive/1_simulation/appendixC/appendix_figureC2.R
@@ -120,18 +120,21 @@ ggCoverPlot = function(var,h=3, w=8){
 			legend.title=element_blank(),
 			axis.ticks=element_blank(),
 			panel.border=element_blank(),
-			axis.text.y=element_text(size=8,
-				family="Source Code Pro Light"),
+			axis.text.y=element_text(size=8
+				#, family="Source Code Pro Light"
+				),
 			axis.text.x=element_text(size=10, face='bold'),
-			strip.text.x = element_text(size=9, color='white',
-				family="Source Code Pro Semibold"),
+			strip.text.x = element_text(size=9, color='white'
+				#, family="Source Code Pro Semibold"
+				),
 			strip.text.y = element_text(size=9, color='white',
-				family="Source Code Pro Semibold", angle=0),
+				# family="Source Code Pro Semibold",
+				angle=0),
 			strip.background = element_rect(fill = "#525252", color='#525252')
 			)
 	ggsave(g, height=3, width=8,
-		file=paste0(graphicsPath, 'appendix_figureC2.pdf'),
-		device=cairo_pdf
+		file=paste0(graphicsPath, 'appendix_figureC2.pdf')
+		#, device=cairo_pdf
 		)
 	return(g)
 }
