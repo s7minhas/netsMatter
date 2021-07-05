52d3f3c62a7a9db88ab8fd4407aba8c1c03c5eeb
s7m <s7.minhas@gmail.com>
2021-07-05 07:37:02 -0400

    final run before dataverse submission

--- replArchive/2_applications/reiter_stam_ameRun.R
+++ replArchive/2_applications/reiter_stam_ameRun.R
@@ -8,6 +8,9 @@ rsPth = paste0(pth, '2_applications/application_data/reiter_stam/')
 # to get same results
 # devtools::install_github('s7minhas/amen', ref='pa2018_version')
 library(amen)
+
+# helper functions
+source(paste0(pth, 'helpers/ame_repL.R'))
 ##############################
 
 ##############################
@@ -18,17 +21,14 @@ load(paste0(rsPth, 'reiterStamData.rda'))
 ##############################
 # run and save
 # ~1.06 days
-startTime = Sys.time()
 ameFit = ame_repL(
   Y=yList,Xdyad=xDyadList,Xrow=NULL,Xcol=NULL,
   model="bin",symmetric=FALSE,intercept=TRUE,
   R=2,
   nscan=100000, seed=6886, burn=50000, odens=10,
-  plot=FALSE, print=FALSE, gof=TRUE,
-  startVals=startVals0
+  plot=FALSE, print=FALSE, gof=TRUE
 )
-endTime = Sys.time()
-print( endTime-startTime )
-save(ameFit, startTime, endTime,
+
+save(ameFit,
   file=paste0(rsPth, 'ameFitReiterStam.rda'))
 ##############################
